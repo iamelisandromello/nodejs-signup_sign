7ab84460868ec5c408a13050fa920d04a5ee7938
Elisandro <elisandro@ulbra.edu.br>
2021-02-23 21:30:07 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,7 @@
 import { Controller } from '../interfaces/controller'
 import { HttpRequest, HttpResponse } from '../interfaces/http'
 import { EmailValidator } from '@/presentation/interfaces/email-validator'
-import { MissingParamError, InvalidParamError } from '@/presentation/errors'
+import { MissingParamError, InvalidParamError, ServerError } from '@/presentation/errors'
 import { badRequest } from '@/presentation/helpers/http/http-helper'
 
 export class SignUpController implements Controller {
@@ -10,24 +10,31 @@ export class SignUpController implements Controller {
   ) {}
 
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+      const { email } = httpRequest.body
+      const isValid = this.emailValidator.isValid(email)
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      const httpResponse = {
+        statusCode: 200,
+        body: { ok: 'ok' }
+      }
+      return httpResponse 
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError()
       }
     }
-    const { email } = httpRequest.body
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
-
-    const httpResponse = {
-      statusCode: 200,
-      body: { ok: 'ok' }
-    }
-    return httpResponse
   }
 }

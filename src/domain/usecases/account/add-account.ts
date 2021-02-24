df04f10c2f375f2899b2b95976c961ade10ecfd1
Elisandro <elisandro@ulbra.edu.br>
2021-02-23 23:52:07 -0300

    feat: add interface AddAccount to assign method add

---
+++ src/domain/usecases/account/add-account.ts
@@ -0,0 +1,6 @@
+import { AccountModel } from '@/domain/models/account'
+export type AddAccountParams = Omit<AccountModel, 'id'>
+
+export interface AddAccount {
+  add: (account: AddAccountParams) => AccountModel
+}

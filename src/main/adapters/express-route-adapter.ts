483fff90b20dfd88df639b477bf35c3a50d7fa18
Elisandro <elisandro@ulbra.edu.br>
2021-02-24 22:25:33 -0300

    refactor: transform handle to async method

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -2,13 +2,13 @@ import { Controller } from '@/presentation/interfaces/controller'
 import { Request, Response } from 'express'
 
 export const adaptRoute = (controller: Controller) => {
-  return (req: Request, res: Response) => {
+  return async (req: Request, res: Response) => {
     const request = {
       body: req.body,
       params: req.params
     }
 
-    const httpResponse = controller.handle(request)
+    const httpResponse = await controller.handle(request)
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
       res.status(httpResponse.statusCode).json(httpResponse.body)
     } else {
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -14,7 +14,7 @@ export class SignUpController implements Controller {
     private readonly addAccount: AddAccount
   ) {}
 
-  handle (httpRequest: HttpRequest): HttpResponse {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
@@ -34,7 +34,7 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('email'))
       }
 
-      const account = this.addAccount.add({
+      const account = await this.addAccount.add({
         name: name,
         email: email,
         password: password

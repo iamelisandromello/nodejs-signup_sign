81f73247eeea3434724107e25e6b6244de5ddd73
Elisandro <elisandro@ulbra.edu.br>
2021-02-24 21:25:06 -0300

    feat: ensure SignUpController returns 500 if AddAccount throws

---
+++ src/presentation/controllers/signup/signup-dependencies.ts
@@ -0,0 +1,4 @@
+export * from '@/presentation/interfaces'
+export * from '@/presentation/interfaces/email-validator'
+export * from '@/domain/usecases/account/add-account'
+export * from '@/domain/models/account'
---
+++ src/presentation/controllers/signup/signup.ts
@@ -0,0 +1,43 @@
+import { Controller, HttpRequest, HttpResponse, EmailValidator, AddAccount } from './signup-dependencies'
+import { MissingParamError, InvalidParamError } from '@/presentation/errors'
+import { badRequest, serverError } from '@/presentation/helpers/http/http-helper'
+
+export class SignUpController implements Controller {
+  constructor (
+    private readonly emailValidator: EmailValidator,
+    private readonly addAccount: AddAccount
+  ) {}
+
+  handle (httpRequest: HttpRequest): HttpResponse {
+    try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+      const { email, name, password, passwordConfirmation } = httpRequest.body
+
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+
+      const isValid = this.emailValidator.isValid(email)
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      this.addAccount.add({
+        name: name,
+        email: email,
+        password: password
+      })
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: serverError(error)
+      }
+    }
+  }
+}
